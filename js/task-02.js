cf9dc3ab283e1ccb84c31b339b752d7702f9f4d7
MaksymMoskalov <max.godnavi133@gmail.com>
2023-07-16 04:00:17 +0300

    Two tests

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.getElementById("ingredients");
+
+const ingredientItems = ingredients.map((element) => {
+  const elementOfIngreds = document.createElement("li");
+  elementOfIngreds.textContent = element;
+  elementOfIngreds.classList.add("item");
+  return elementOfIngreds;
+});
+
+listEl.append(...ingredientItems);
