e64dcb249473cb9e97b2622daa036295a26902a9
MaksymMoskalov <max.godnavi133@gmail.com>
2023-07-17 04:36:54 +0300

    small bug

--- js/task-07.js
+++ js/task-07.js
@@ -4,6 +4,5 @@ const textEl = document.querySelector("#text");
 InputEl.addEventListener("input", onFontChanger);
 
 function onFontChanger() {
-  console.dir(textEl.style);
   textEl.style.fontSize = `${InputEl.value}px`;
 }
